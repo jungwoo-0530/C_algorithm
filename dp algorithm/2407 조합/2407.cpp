fc77050385c01eccbfa1923e416136216a59e9e7
jungwoo-0530 <am7227@naver.com>
2021-01-25 10:44:17 +0900

    dp

--- dp algorithm/2407 조합/2407.cpp
+++ dp algorithm/2407 조합/2407.cpp
@@ -6,16 +6,21 @@ using namespace std;
 // 
 
 int n,m;//n>=m, 5<=n<=100 100!팩토리얼을 저장.
+//m,n >= 5.
 unsigned long long factorial[101];
+long long dp[101][101];
 //factorial[i] = factorial[i-1]*i;
-
+//fact[n][m] = 
 void solve()
 {
     factorial[0] = 0;
     factorial[1] = 1;
-    for(int i=2; i<=100; i++)
+    for(int i=5; i<=100; i++)
     {
-        factorial[i] = factorial[i-1]*i;
+        for(int j=5; j<=i; j++)
+        {
+            dp[i][j] = 
+        }
     }
 
     cin >> n >> m;
