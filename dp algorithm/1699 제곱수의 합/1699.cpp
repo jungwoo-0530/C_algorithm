1d396978cfc265e97c300513e98a35defc25b138
jungwoo-0530 <am7227@naver.com>
2021-01-28 22:38:32 +0900

    dp

--- dp algorithm/1699 제곱수의 합/1699.cpp
+++ dp algorithm/1699 제곱수의 합/1699.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int dp[100001];
 
 //짝수 + 홀수인경우 1을 사용.
-//1. 1 ,2,3,4,5..의 제곱수는 무조건 dp[k]가 1 -> 4%2 = 0, 9
-// 12 = 9+2+1 4+4+4
-//2. 
+//1. 제곱인 수는 무조건 1
+//2. 8인 수를 보면 
+//  1+1+1+1+1+1+1+1은 1의 경우의 수에서 7개 항을 더한 것이므로 8
+//  4+4은 dp[4] + 1
+// dp[8] = min(dp[7]+1, dp[4])
+
+int min1(int a, int b){if(a > b)return b; else return a;}
+
 void solve()
 {
     int N;
@@ -16,20 +22,14 @@ void solve()
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 3;
-    
-    for(int i=4; i<=100000;i++)
+
+    for(int i=4; i<=N;i++)
     {
-        if(i)
-        {
-
-        }
-
-        else
-        {
-            dp[i] = dp[i-1] + 1;
-        }
-        
+        dp[i] = i;
+        for(int j=1; j<=sqrt(i); j++)
+            dp[i] = min1(dp[i], dp[i-j*j]);
     }
+    
 
     cout << dp[N];
 
