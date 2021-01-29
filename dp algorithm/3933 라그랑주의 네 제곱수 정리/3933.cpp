a9f387acabc371b5ef72e679a8f4b36e1d5b33bf
jungwoo-0530 <am7227@naver.com>
2021-01-29 23:21:21 +0900

    dp

---
+++ dp algorithm/3933 라그랑주의 네 제곱수 정리/3933.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+int dp[32769];
+//2^15 = 32768
+//라그랑주의 네 제곱 : 모든 수는 4개의 제곱수로 표현
+// 3 = 1^2+1^2+1^2+0^2
+//
+//
+
+void solve()
+{
+
+    while(1)
+    {
+        int a;
+        cin >> a;
+        if(a==0)break;
+        
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
