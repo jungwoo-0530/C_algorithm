c0bd5a3e97c2813524de7e7c8edf23b2b25bd8e1
jungwoo-0530 <am7227@naver.com>
2021-02-21 12:28:36 +0900

    update

--- string/1786 찾기(kmp)/1786.cpp
+++ string/1786 찾기(kmp)/1786.cpp
@@ -3,21 +3,27 @@
 
 using namespace std;
 
-vector<int> failfun(string a)
+//LPS를 구할때는 j,i로 시작한다.
+//j는0에서시작 i는 1에서 시작, j는 접두사, i는 접미사
+//i,j일치하는지 확인, i는 매번 1씩 증가
+//i인덱스와 j인덱스 문자가 다를 경우, j인덱스의 값으로 이동하고 j와 i인덱스 비교
+//i인덱스 문자와 j인덱스 문자가 같을 경우, j+1을 i인덱스값에넣는다.
+vector<int> failfun(string a)//LPS
 {
     int size = (int)a.size();
     int j = 0;
-
+    int i = 1;
     vector<int>fail(size,0);
 
-    for(int i=1; i<size; i++)
+    for(i; i<size; i++)
     {
-        while()
-        if(a[i] == a[j])
+        while(j>0 && a[i] != a[j])
         {
-            j++;
-            fail[i] = j;
+            j = fail[j-1];
         }
+        if(a[i] == a[j])//같다면 j를 증가시키고 j값을 저장.
+            fail[i] = ++j;
+        
      }
     
     return fail;
