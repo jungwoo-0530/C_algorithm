9ac720bcdefc9be8471c41161517837b1d4573dd
jungwoo-0530 <am7227@naver.com>
2022-01-25 08:36:39 +0900

    algorithm

--- DFS BFS/프로그래머스/카카오 /2020/기둥과 보 설치/main.cpp
+++ DFS BFS/프로그래머스/카카오 /2020/기둥과 보 설치/main.cpp
@@ -1,101 +1,103 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-//x(가로) y(세로) a(0 기둥 1 보) b(0삭제, 1설치)
-vector<vector<int> > solution(int n, vector<vector<int> > build_frame1)
+// x(가로) y(세로) a(0 기둥 1 보) b(0삭제, 1설치)
+
+bool oneCheck(const set<vector<int>> &building, vector<int> pred)
 {
-    vector<vector<int> > answer(7, vector<int>(3, 0));
-    answer.clear();
-    //first 기둥, second 보.
-    vector<vector<pair<bool,bool> > > map;
-    vector<vector<int> > build_frame(6, vector<int>(4,0));
-    
-    build_frame[0][0]= 1;
-    build_frame[0][1]= 0;
-    build_frame[0][2]= 0;
-    build_frame[0][3]= 1;
+    if (building.find(pred) != building.end())
+        return true;
+    return false;
+}
 
-    build_frame[1][0] = 1;
-    build_frame[1][1] = 1;
-    build_frame[1][2] = 1;
-    build_frame[1][3] = 1;
-
-    build_frame[2][0] = 1;
-    build_frame[2][1] = 1;
-    build_frame[2][2] = 1;
-    build_frame[2][3] = 1;
-
-    build_frame[3][0] = 2;
-    build_frame[3][1] = 1;
-    build_frame[3][2] = 1;
-    build_frame[3][3] = 1;
-
-    build_frame[4][0] = 3;
-    build_frame[4][1] = 1;
-    build_frame[4][2] = 1;
-    build_frame[4][3] = 1;
-
-    build_frame[5][0] = 4;
-    build_frame[5][1] = 0;
-    build_frame[5][2] = 0;
-    build_frame[5][3] = 1;
-
-    cout << build_frame[0][3];
-    cout << build_frame[5][0];
-    cout << build_frame.size();
-    for(int i = 0; i < 3; i++)
+bool check(const set<vector<int>> &building)
+{
+    for (auto build : building)
     {
-        int x = build_frame[i][0];
-        int y = build_frame[i][1];
+        int x = build[0];
+        int y = build[1];
+        int a = build[2];
 
-        if(build_frame[i][3] == 1)//설치
+        if (a == 0)
         {
-            if (build_frame[i][2] == 0)//기둥
-            {
-                if((y == 0) || (map[y][x-1].second == true) || map[y-1][x].first == true)
-                {   
-                    answer[i].push_back(x);
-                    answer[i].push_back(y);
-                    answer[i].push_back(build_frame[i][2]);
-                }
-            }
-            else//보
-            {
-                if(map[y-1][x-1].first == true || (map[y][x-1].second == true && map[y][x+1].second == true)){
-                    answer[i].push_back(x);
-                    answer[i].push_back(y);
-                    answer[i].push_back(build_frame[i][2]);
-                }
-            }
+            vector<int> pred[3] = {{x, y - 1, 0}, {x, y, 1}, {x - 1, y, 1}};
+            if (y == 0 || oneCheck(building, pred[0]) || oneCheck(building, pred[1]) || oneCheck(building, pred[2]))
+                continue;
+            return false;
         }
-
-        else//삭제
+        else
         {
-            if (build_frame[i][2] == 0) //기둥
-            {
-            }
-            else //보
-            {
-            }
+            vector<int> pred[4] = {{x, y - 1, 0}, {x + 1, y - 1, 0}, {x + 1, y, 1}, {x - 1, y, 1}};
+            if (oneCheck(building, pred[0]) || oneCheck(building, pred[1]) || (oneCheck(building, pred[2]) && oneCheck(building, pred[3])))
+                continue;
+            return false;
         }
     }
-    cout << endl;
-    for(int i = 0; i < answer.size(); i++)
+    return true;
+}
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame)
+{
+    vector<vector<int>> answer;
+    set<vector<int>> building;
+
+    for (int i = 0; i < build_frame.size(); i++)
     {
-        cout << answer[i][0] << " " << answer[i][1] << " " << answer[i][2] << endl;
+        vector<int> v = {build_frame[i][0], build_frame[i][1], build_frame[i][2]};
+        if (build_frame[i][3] == 0) //삭제
+        {
+            building.erase(v);
+            if (!check(building))
+                building.insert(v);
+        }
+
+        else
+        {
+            building.insert(v);
+            if (!check(building))
+                building.erase(v);
+        }
     }
 
+    for (auto build : building)
+    {
+        answer.push_back(build);
+    }
 
     return answer;
 }
 
 int main()
 {
-    vector<vector<int> > a;
-    a = solution(6, a);
+
+    vector<int> a{1, 0, 0, 1};
+    vector<int> b{1, 1, 1, 1};
+    vector<int> c{2, 1, 0, 1};
+    vector<int> d{2, 2, 1, 1};
+    vector<int> e{5, 0, 0, 1};
+    vector<int> f{5, 1, 0, 1};
+    vector<int> g{4, 2, 1, 1};
+    vector<int> q{3, 2, 1, 1};
+
+    vector<vector<int>> temp;
+    temp.push_back(a);
+    temp.push_back(b);
+    temp.push_back(c);
+    temp.push_back(d);
+    temp.push_back(e);
+    temp.push_back(f);
+    temp.push_back(g);
+    temp.push_back(q);
+
+    vector<vector<int>> z;
+    z = solution(8, temp);
+
+    //    a = solution(6, a);
 
     return 0;
 }
